25bde23fde6c1992e7e9182d55e78ce3d2bb7640
VanderV8 <vanderlima3@gmail.com>
2023-07-09 01:02:42 -0300

    Corrigindo a atividade 4

--- projetopwc.py
+++ projetopwc.py
@@ -1,3 +1,16 @@
+"""
+Projeto PWC manipulação de strings
+Descrição: Este programa faz 5 tipos diferentes de  manipulação de strings, sendo elas:
+                1.Faça um código que reverta a ordem das palavras nas frases, mantendo a ordem das palavras.
+                2 Remova todos os caracteres duplicados da string abaixo.
+                3 Encontre a substring palindromo mais longa na string abaixo.
+                4 Coloque em maiúsculo a primeira letra de cada frase na string.
+                5 Verificar se a string é um anagrama de um palindromo:
+Autor: Vander Lima De Assis
+Data: 9 de Julho de 2023
+
+"""
+
 # 1.Faça um código que reverta a ordem das palavras nas frases, mantendo a ordem das palavras.
 def inverter_texto(texto):
 
